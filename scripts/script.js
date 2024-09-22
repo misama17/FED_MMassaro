961f58d2832dac05a8ceb7839f1b28e3ff9105b3
misama17 <milamassaro17@gmail.com>
2024-09-22 13:09:04 +0200

    test upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,62 @@
 // CONSTANTEN
-const slides = document.querySelectorAll('section:nth-of-type(2) li');
-const previousButton = document.querySelector('section:nth-of-type(2) button:nth-of-type(1)');
-const nextButton = document.querySelector('section:nth-of-type(2) button:nth-of-type(2)');
+const slides1 = document.querySelectorAll('section:nth-of-type(2) li');
+const previousButton1 = document.querySelector('section:nth-of-type(2) button:nth-of-type(1)');
+const nextButton1 = document.querySelector('section:nth-of-type(2) button:nth-of-type(2)');
+const slides2 = document.querySelectorAll('section:nth-of-type(4) li');
+const previousButton2 = document.querySelector('section:nth-of-type(4) button:nth-of-type(1)');
+const nextButton2 = document.querySelector('section:nth-of-type(4) button:nth-of-type(2)');
 
 // VARIABELEN
-let currentSlide = 0;
-let totalSlides = slides.length;
+let currentSlide1 = 0;
+let totalSlides1 = slides1.length;
+let currentSlide2 = 0;
+let totalSlides2 = slides2.length;
 
 // CAROUSEL SLIDE SHOW
 // CHATGPT >> vraag: ik wil een carousel maken waarbij je naar het volgende en vorige plaatje kunt gaan door te klikken op een button.
-function slideShow(index) {
-    // Verander het indexnummer om ervoor te zorgen dat we binnen de grenzen blijven
-    if (index >= totalSlides) {
-      currentSlide = 0; // Ga terug naar de eerste afbeelding
+function slideShow1(index) {
+    if (index >= totalSlides1) {
+      currentSlide1 = 0; // Ga terug naar de eerste afbeelding
     } else if (index < 0) {
-      currentSlide = slides.length - 1; // Ga naar de laatste afbeelding
+      currentSlide1 = totalSlides1 - 1; // Ga naar de laatste afbeelding
     } else {
-      currentSlide = index;
+      currentSlide1 = index;
     }
 
     // ChatGPT
-    document.querySelector('section:nth-of-type(2) > section:nth-of-type(2)').style.transform = `translateX(-${currentSlide * 96}%)`;
+    document.querySelector('section:nth-of-type(2) > section:nth-of-type(2)').style.transform = `translateX(-${currentSlide1 * 100}%)`;
 }
 
-function nextSlide() {
-    slideShow(currentSlide + 1);
+function nextSlide1() {
+  slideShow1(currentSlide1 + 1);
 }
   
-  function previousSlide() {
-    slideShow(currentSlide - 1);
+function previousSlide1() {
+  slideShow1(currentSlide1 - 1);
 }
 
-previousButton.onclick = previousSlide; 
-nextButton.onclick = nextSlide;
+function slideShow2(index) {
+  if (index >= totalSlides2) {
+    currentSlide2 = 0; // Ga terug naar de eerste afbeelding
+  } else if (index < 0) {
+    currentSlide2 = totalSlides2 - 1; // Ga naar de laatste afbeelding
+  } else {
+    currentSlide2 = index;
+  }
 
+  // ChatGPT
+  document.querySelector('section:nth-of-type(4) > section:nth-of-type(2)').style.transform = `translateX(-${currentSlide2 * 100}%)`;
+}
 
+function nextSlide2() {
+  slideShow2(currentSlide2 + 1);
+}
+
+function previousSlide2() {
+  slideShow2(currentSlide2 - 1);
+}
+
+previousButton1.onclick = previousSlide1; 
+nextButton1.onclick = nextSlide1;
+previousButton2.onclick = previousSlide2; 
+nextButton2.onclick = nextSlide2;
